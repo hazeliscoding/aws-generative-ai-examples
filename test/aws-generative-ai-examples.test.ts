48a46b7c0231ed8be91191af829939347bb221a2
hazeliscoding <duck_soop@outlook.com>
2024-06-03 21:31:08 -0500

    Initial commit

---
+++ test/aws-generative-ai-examples.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsGenerativeAiExamples from '../lib/aws-generative-ai-examples-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-generative-ai-examples-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsGenerativeAiExamples.AwsGenerativeAiExamplesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
